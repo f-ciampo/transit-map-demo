ece501efcf1af53c4b66dfd5ba20ae181a1e1bf6
f-ciampo <francisco@ciampo.ar>
2026-05-11 13:29:43 -0300

    Add search ui

---
+++ ui.js
@@ -0,0 +1,180 @@
+class Autocomplete {
+  constructor(
+    container,
+    getSuggestions,
+    onAccept,
+    defaultText,
+    delay = 300
+  ) {
+    this.container = container;
+    this.getSuggestions = getSuggestions;
+    this.onAccept = onAccept;
+
+    this.suggestions = [];
+    this.selectingIndex = -1;
+
+    this.inputElement = document.createElement("input");
+    this.inputElement.autocomplete = "off";
+
+    this.listElement = document.createElement("div");
+    this.listElement.className = "suggestions";
+    this.listElement.hidden = true;
+
+    this.container.appendChild(this.inputElement);
+    this.container.appendChild(this.listElement);
+
+    this.delay = delay;
+    this.completeTimeout = null;
+
+    this.inputElement.placeholder = defaultText;
+
+    this.loading = false;
+
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.inputElement.addEventListener("input", () => {
+      clearTimeout(this.completeTimeout);
+      
+      this.loading = true;
+      this.renderSuggestions();
+
+      this.completeTimeout = setTimeout(() => {
+        this.completeText();
+      }, this.delay);
+    });
+
+    this.inputElement.addEventListener("keydown", e => {
+      if (e.key === "ArrowDown") {
+        e.preventDefault();
+
+        this.selectingIndex =
+          Math.min(this.selectingIndex + 1, this.suggestions.length - 1);
+
+        this.renderSuggestions();
+      }
+
+      else if (e.key === "ArrowUp") {
+        e.preventDefault();
+
+        this.selectingIndex = Math.max(this.selectingIndex - 1, 0);
+
+        this.renderSuggestions();
+      }
+
+      else if (e.key === "Enter") {
+        e.preventDefault();
+
+        if (this.selectingIndex >= 0) {
+          this.accept(
+            this.suggestions[this.selectingIndex]
+          );
+        } else if (this.suggestions.length) {
+          this.accept(this.suggestions[0]);
+        }
+      }
+
+      else if (e.key === "Tab") {
+        if (this.suggestions.length) {
+          e.preventDefault();
+
+          const completion = this.selectingIndex >= 0 ?
+            this.suggestions[this.selectingIndex] :
+            this.suggestions[0];
+
+          this.inputElement.value = completion.text + completion.afterText;
+
+          this.completeText();
+        }
+      }
+
+      else if (e.key === "Escape") {
+        this.inputElement.blur();
+        this.listElement.hidden = true;
+      }
+    });
+
+    document.addEventListener("click", e => {
+      if (!e.target.closest(".autocomplete")) {
+        this.listElement.hidden = true;
+      }
+    });
+
+    this.inputElement.addEventListener("focus", () => {
+      this.completeText();
+    });
+  }
+  
+  async completeText() {
+    this.suggestions =
+      await this.getSuggestions(this.inputElement.value) || [];
+
+    this.loading = false;
+
+    this.selectingIndex = -1;
+
+    this.renderSuggestions();
+  }
+
+  renderSuggestions() {
+    this.listElement.innerHTML = "";
+    if (!this.inputElement.value.length) return;
+
+    if (!this.suggestions.length) {
+      const div = document.createElement("div");
+
+      div.className = "suggestion text-muted";
+      div.textContent = this.loading ? "Buscando..." : "Sin resultados";
+
+      this.listElement.appendChild(div);
+      this.listElement.hidden = false;
+
+      return;
+    }
+
+    this.suggestions.forEach((suggestion, i) => {
+      const { item, text, afterText } = suggestion;
+      const div = document.createElement("div");
+
+      div.className =
+        "suggestion" + (i === this.selectingIndex ? " selecting" : "");
+
+      const main = document.createElement("span");
+      main.textContent = text;
+
+      const after = document.createElement("span");
+      after.textContent = afterText;
+      after.className = "text-muted";
+
+      div.appendChild(main);
+      div.appendChild(after);
+
+      div.onclick = () => this.accept(suggestion);
+
+      if (i === this.selectingIndex) {
+        requestAnimationFrame(() => {
+          div.scrollIntoView({
+            block: "nearest"
+          });
+        });
+      }
+
+      this.listElement.appendChild(div);
+    });
+
+    this.listElement.hidden = false;
+  }
+
+  accept(value) {
+    //this.inputElement.value = value.text;
+    this.inputElement.value = "";
+
+    this.suggestions = [];
+    this.selectingIndex = -1;
+
+    this.renderSuggestions();
+
+    this.onAccept?.(value);
+  }
+}
